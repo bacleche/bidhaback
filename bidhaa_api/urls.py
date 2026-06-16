deb56bc68e1e86b7e4531def727658c30ad773a2
bacleche <schadracmoualou@gmail.com>
2026-06-16 14:34:43 +0100

    add def on setting project

--- bidhaa_api/urls.py
+++ bidhaa_api/urls.py
@@ -3,6 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import JsonResponse
+
+def debug_cloudinary(request):
+    return JsonResponse({
+        'cloud_name': os.environ.get('CLOUDINARY_CLOUD_NAME', 'NON DÉFINI'),
+        'api_key': os.environ.get('CLOUDINARY_API_KEY', 'NON DÉFINI'),
+        'has_secret': bool(os.environ.get('CLOUDINARY_API_SECRET')),
+    })
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
@@ -11,5 +19,7 @@ urlpatterns = [
     path('api/', include('clients.urls')),
     path('api/', include('transactions.urls')),
     path('api/', include('interactions.urls')),
+
+    path('debug-cloudinary/', debug_cloudinary),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
