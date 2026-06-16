de47a955a6cf67df8bcef5b85ecba0aed704aee8
bacleche <schadracmoualou@gmail.com>
2026-06-16 10:01:51 +0100

    update du conf with front

--- bidhaa_api/settings.py
+++ bidhaa_api/settings.py
@@ -52,6 +52,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://bidhafront.vercel.app", # L'URL de votre front Vercel
+]
 CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
